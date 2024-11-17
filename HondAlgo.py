bffa43ae84c2e119957e4ff24acc3573d1fa9e85
Mohamed Hossam <53664989+mohamedhossamtharwat@users.noreply.github.com>
2024-11-17 07:59:31 +0000

    hyperlink

--- HondAlgo.py
+++ HondAlgo.py
@@ -201,6 +201,7 @@ st.markdown(
             text-decoration: none;
         }
         .footer-center a:hover {
+            color: blue;
             text-decoration: underline;
         }
     </style>
